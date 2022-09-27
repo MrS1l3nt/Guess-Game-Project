10ae9b51c312d6c93acaca68858818cbbd6d184e
Farid Hajiyev <104371279+MrS1l3nt@users.noreply.github.com>
2022-09-27 20:16:22 +0400

    update v1.0
    Now everything works fine :)

--- guess.sh
+++ guess.sh
@@ -6,20 +6,26 @@ read guess
 let guessnum=1
 while (( guess != random ))
 do
-	if (( guess > random ))
-	then
-		echo "Number is smaller than you expected!"
-	elif (( guess < random ))
-	then
-		echo "Number is greater than you expected!"
-	fi
-	echo -e "Enter the number between 1 and 100:\c"
-	read guess
-	let guessnum++
+        while ! [[ $guess =~ ^[[:blank:]]*[0-9]*[[:blank:]]*$ ]]
+        do
+                echo "You didn't enter number!!!"
+                echo -e "Please, enter number between 1 to 100:\c"
+                read guess
+        done
+        if (( guess > random ))
+        then
+                echo "Number is smaller than you expected!"
+        elif (( guess < random ))
+        then
+                echo "Number is greater than you expected!"
+        fi
+        echo -e "Enter the number between 1 and 100:\c"
+        read guess
+        let guessnum++
 done
 if [ $guessnum -lt 2 ]
 then
-	echo "You guess the number after $guessnum try..."
+        echo "You guess the number after $guessnum try..."
 else
-	echo "You guess the number after $guessnum tries..."
+        echo "You guess the number after $guessnum tries..."
 fi
